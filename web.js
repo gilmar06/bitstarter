da284f7327d615d6c3dd3a09aa84424d2676cc82
Cesar Caslini <cesarcaslini@gmail.com>
2013-07-13 23:09:51 +0000

    Trying to read from index.html

--- web.js
+++ web.js
@@ -1,11 +1,12 @@
 var express = require('express');
 
+var fs=require('fs');
+
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
     var buffer = new Buffer(0);
     buffer = fs.readFileSync("index.html");
-    
     response.send(buffer.toString());
 });
 
