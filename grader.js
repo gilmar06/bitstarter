a2b610c1523d705102039bb89b9adb55b2c246f5
Cesar Caslini <cesarcaslini@gmail.com>
2013-07-16 21:19:14 +0000

    after some help

--- grader.js
+++ grader.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-
 /*
 Automatically grade files for the presence of specified HTML tags/attributes.
 Uses commander.js and cheerio. Teaches command line application development
@@ -25,10 +24,10 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var restler = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 var checkJson;
-var restler = require('restler');
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -47,10 +46,10 @@ var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-// Download URL content and call the verification function
+// Download URL content and call verification function
 var loadURL = function(urladdress, checksfile){
-    restler.get(urladdress).on('complete',function(data){
-	checkJson = checkHtmlFile(data.toString(),checksfile);
+    restler.get(urladdress).on('complete', function(data){
+	checkJson = checkHtmlFile(data.toString(), checksfile);
     })
 };
 
@@ -75,13 +74,13 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-	.option('-u, --url <url_address>', 'URL address') // 1st try to get an URL
+	.option('-u, --url <url_address>', 'URL address')
         .parse(process.argv);
-    if (program.file){
-	checkJson = checkHtmlFile(program.file, program.checks);
+    if(program.file){
+	var checkJson = checkHtmlFile(program.file, program.checks);
     }
-    if (program.url){
-	loardURL(program.url, program.checks);
+    if(program.url){
+	loadURL(program.url, program.checks);
     }
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
